db5a43d86d1ef3cd86a70a533f0357a3582bcb94
mwdacus <mwdacus@gmail.com>
2022-05-06 12:36:33 -0700

    Process and Split Data

--- rbfkernel/RBFMain.jl
+++ rbfkernel/RBFMain.jl
@@ -8,15 +8,44 @@
     #Resulting Plot of Interference Region based on Kernel Function
 #----------------------------------------------------------------------------------
 
-
+#CURRENT ISSUE: NEED TO RUN DATA ON GPU (PARALLEL COMPUTING)
 
 using CSV
 using DataFrames
 using LinearAlgebra
-using Main.Util_Data
+using KernelFunctions
+using LIBSVM
+using Main.DataProcess
 
-include("Util_Data.jl")
+#Train Kernel
+function TrainKernel(trainingdata)
+    k=SqExponentialKernel()∘ ScaleTransform(1.5)
+    model = svmtrain(kernelmatrix(k, trainingdata.X), trainingdata.Y; kernel=LIBSVM.Kernel.Precomputed)
+    test_range = range(floor(Int, minimum(trainingdata.X)), ceil(Int, maximum(trainingdata.X)); length=100)
+    return model, test_range, k
+end
 
+#Validate Kernel
+function ValidateKernel(test_range,trainingdata,valdata,k)
+    x_val = ColVecs(mapreduce(collect, hcat, Iterators.product(test_range, test_range)));
+    y_pred, _ = svmpredict(model, kernelmatrix(k, trainingdata.X, valdata.X));
+    return x_val, y_pred
+end
+
+#Main Script
 #Import Data
+filename,rawdata=DataProcess.FileUpload()
+#Organize and Split into training and validation data, and features/labels
+datat,datav=DataProcess.SplitData(rawdata)
+#Train Model on Training Data
+#model,test_range,k=TrainKernel(datat)
+#Validate Model
+#x_val,y_pred=ValidateKernel(test_range,datat,datav,k)
 
-Util_Data.fileupload()
+
+
+
+#Plot Data
+#DataProcess.PlotADSB(datat,datav,model)
+
+
--- rbfkernel/Util_Data.jl
+++ rbfkernel/Util_Data.jl
@@ -10,24 +10,57 @@
     #Data split in between features and labels
 #----------------------------------------------------------------------------------
 module DataProcess
+
 using CSV
 using DataFrames
+using KernelFunctions
+using Random
+using Gtk
+using Plots
 
 struct Data
-    X::Array{Vector{Float64}}
-    Y::Float64
+    X::RowVecs{Float64, Matrix{Float64}, SubArray{Float64, 1, Matrix{Float64}, 
+    Tuple{Int64, Base.Slice{Base.OneTo{Int64}}}, true}}         #Features
+    Y::Vector{Float64}                                          #Labels
 end
 
 
 #Import Data
-function fileupload()
-    filename=open_dialog("My Open dialog")
+function FileUpload()
+    filename=open_dialog("Upload ADS-B File")
     rawdata=DataFrame(CSV.File(filename))
     return filename,rawdata
 end
 
-#Plot Data
+#Create Validation Data Set
+function CreateDataSet(data)
+    A=[data.lat data.lon data.alt]
+    B=data.nic
+    ind2=findall(x->x>=7,B)
+    ind1=findall(x->3<=x<=6,B)
+    ind0=findall(x->0<=x<=2,B)
+    B[ind2].=2
+    B[ind1].=1
+    B[ind0].=0
+    return Data(RowVecs(A),B)
+end
 
+#Split Data
+function SplitData(rawdata)
+    n=size(rawdata,1)
+    shuffledata=rawdata[shuffle(1:end),:]
+    splitind=Int(round(0.2*n))
+    trainingdata=shuffledata[1:splitind,:]
+    valdata=shuffledata[splitind+1:end,:]
+    datat=CreateDataSet(trainingdata)
+    datav=CreateDataSet(valdata)
+    return datat,datav
+end
+
+#Plot Data
+function PlotADSB()
+
+end
 
 end
 
