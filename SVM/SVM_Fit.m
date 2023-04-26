fb4d5fd069ed7a4ccf70e4005018f0b357df87b1
mwdacus <mwdacus@gmail.com>
2023-04-26 12:18:33 -0700

    add svm localization method

--- SVM/SVM_Fit.m
+++ SVM/SVM_Fit.m
@@ -10,34 +10,43 @@
 
 %************************************************************************
 
-function SVM_Fit(eventdata)
+function SVM_Fit(adsbdata,Z,RZ,boxalt,origin)
     %Filter Data
-    filt_data=Filter_Data(eventdata.adsbdata,eventdata.Z,eventdata.RZ);
+    filt_data=Filter_Data(adsbdata,Z,RZ,boxalt,origin);
     %Implement Density Weighting Scheme
-    weight_data=Density_Weight(filt_data);
-    %Split Airspace into 10 layers (by altitude)
-    layerheight=linspace(eventdata.boxalt(1),eventdata.boxalt(2),10);
-    nlayers=numel(layerheight)-1;
+    filt_data.Weights=Density_Weight(filt_data);
+    %Split Data into 
+    %Split Airspace into layers every 1000'
+    layerint=boxalt(1)*3.28:1000:boxalt(2)*3.28;
+    nlayers=numel(layerint)-1;
     %Determine Decision Boundary for each layer
     bdata=cell(1,nlayers);
     for h=1:nlayers
-        heightdata=weight_data((weight_data<=layerheight(h) && weight_data>=layerheight(h+1)),:);
-        bdata(h)=DecisionBoundary(heightdata);
+        layerdata=filt_data((filt_data.Weights<=layerint(h) &&...
+            filt_data.Weights>=layerint(h+1)),:);
+        bdata(h)=DecisionBoundary(layerdata);
     end
     %plot data
-    fig=ADSBtools.plot.plot_3Dterrain(weight_data,eventdata.RZ);
-    %plot boundary
-    Plot_Boundary(fig,bdata)
+    Plot_Boundary(bdata)
 end
 
 %% Other Functions 
 %Find Decision Boundary for a set of data using Support Vector Machine
 %(SVM)
-function DecisionBoundary()
-
+function [h]=DecisionBoundary(layerdata)
+    Tbl=layerdata(:,ismember(layerdata.Properties.VariableNames, ...
+        {'lat','lon','alt','Weights'}));
+    Y=layerdata.nic(layerdata.nic<=7);    
+    model=fitcsvm(Tbl,Y,'KernelFunction','rbf','BoxConstraint',inf);
+    [~,score]=predict(model,Tbl);
+    h=contour(x1,x2,reshape(score(:,2),size(x1)),[0 0],'k');
 end
 
 %Plot function for adding decision boundaries
 function Plot_Boundary()
-
+    ADSBtools.plot.plot_3Dterrain(w,eventdata.RZ);
+    hold on
+    for i=1:numel(bdata)
+        
+    end
 end
