6987f1a9aeb6a6b39bb7e0de665c9c71a53a0246
Michael Dacus <mwdacus@gmail.com>
2023-05-05 10:45:58 -0700

    modify weights

--- util/Density_Euclid.m
+++ util/Density_Euclid.m
@@ -10,14 +10,14 @@
 
 function [w_norm]=Density_Euclid(x,y,alt)
     data=[x,y,alt];
-    r=100;
+    r=10000;
     n=size(data,1);
     dist=zeros(n,1);w=zeros(n,1);
     for i=1:n
         pointi=[data(i,1) data(i,2) data(i,3)];
         dist=sum((pointi.*ones(n,3)-data).^2,2).^(.5);
         %Calculate weight at point i
-        inside_rad=sum(dist>=r);
+        inside_rad=sum(dist<=r);
         w(i)=inside_rad/n;
     end
     %normalize weights
